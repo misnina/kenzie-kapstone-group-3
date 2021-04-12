c7cd4c16fba8b2fd1a8611cf71ad75181da6272f
Lorion <lorion.m.brown@outlook.com>
2021-04-12 14:47:15 -0400

    added components and did all importing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import Menu from "./components/Menu";
 import './styles/App.scss';
 import Channel from "./views/Channel";
 
+import Home from './views/Home/Home'
+import Profile from './views/Profile/Profile'
+import Message from './components/Message/Message'
+import General from './views/General/General'
+import Books from './views/Books/Books'
+import Tv from './views/Tv/Tv'
+import Gaming from './views/Gaming/Gaming'
+
 
 function App() {
   return (
@@ -9,6 +17,8 @@ function App() {
       <Menu />
       <div id="main-area">
          <Channel />
+
+         
       </div>
     </div>
   );
