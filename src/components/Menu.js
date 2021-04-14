7345dc5139b5151e71a320216667b4bf8ead8ff6
Lorion <lorion.m.brown@outlook.com>
2021-04-13 23:42:01 -0400

    added links to each page now have error in app js page

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -4,11 +4,20 @@ import '../styles/Menu.scss';
 export default function Menu() {
   return (
     <div id="Menu">
+      <h1>things</h1>
       <div id="top-menu">
-        things
+        <Link to="/profile">Profile</Link>
+        <Link to="/">Home</Link>
       </div>
       <div id="lower-menu">
-        bottom things
+        <h1>bottom things</h1>
+        <Link to="/general">General</Link>
+        <Link to="/books">Books</Link>
+        <Link to="/tv">Tv</Link>
+        <Link to="/gaming">Gaming</Link>
+
+        <button onClick={logout}>Logout</button>
+
       </div>
     </div>
   )
