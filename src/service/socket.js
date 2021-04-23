43140677d986c20ba350e81eb5c9f7b996947a74
Nina Buie <ninarachaebuie@gmail.com>
2021-04-23 17:53:56 -0500

    method

--- src/service/socket.js
+++ src/service/socket.js
@@ -2,6 +2,11 @@ import io from "socket.io-client";
 import React from 'react';
 
 export const socket = io("https://dry-spire-38380.herokuapp.com/", {
-  withCredentials: true
+  withCredentials: true,
+  extraHeaders: {
+    "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE",
+    "Access-Control-Allow-Origin": "https://dry-spire-38380.herokuapp.com",
+    "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
+  }
 });
 export const SocketContext = React.createContext(socket);
