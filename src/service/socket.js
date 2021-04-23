07563a3a6aea61c5cf1541ef41d56e73e0dbac8a
Nina Buie <ninarachaebuie@gmail.com>
2021-04-23 18:44:15 -0500

    complete frontend

--- src/service/socket.js
+++ src/service/socket.js
@@ -1,12 +1,11 @@
 import io from "socket.io-client";
 import React from 'react';
 
-export const socket = io("https://dry-spire-38380.herokuapp.com/", {
-  withCredentials: true,
+export const socket = io("http://dry-spire-38380.herokuapp.com", {
   extraHeaders: {
     "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE",
-    "Access-Control-Allow-Origin": "https://dry-spire-38380.herokuapp.com",
-    "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
+    "Access-Control-Allow-Origin": "http://dry-spire-38380.herokuapp.com",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization, Accept",
   }
 });
 export const SocketContext = React.createContext(socket);
