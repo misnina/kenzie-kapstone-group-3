1359c16b2d6ce8cbe5ea6eec7a1b406f3e9857cc
Nina Buie <ninarachaebuie@gmail.com>
2021-04-23 19:05:38 -0500

    httpps more

--- src/service/socket.js
+++ src/service/socket.js
@@ -4,7 +4,7 @@ import React from 'react';
 export const socket = io("https://dry-spire-38380.herokuapp.com", {
   extraHeaders: {
     "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE",
-    "Access-Control-Allow-Origin": "http://dry-spire-38380.herokuapp.com",
+    "Access-Control-Allow-Origin": "https://dry-spire-38380.herokuapp.com",
     "Access-Control-Allow-Headers": "Content-Type, Authorization, Accept",
   }
 });
